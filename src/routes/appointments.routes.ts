f2ffd8f0d4d4e8e586ddb3589031272cd8eca665
JosephMartins <neto-neon@live.com>
2020-09-18 16:32:14 -0300

    Feature: implementacao de repositories

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,29 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointmentRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+  const parsedDate = startOfHour(parseISO(date));
 
-  appointments.push(appointment);
+  const findAppointmentInSameDate = appointmentRepository.findByDate(
+    parsedDate,
+  );
+
+  console.log(findAppointmentInSameDate);
+
+  if (findAppointmentInSameDate === null) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentRepository.create(provider, date);
 
   return response.json(appointment);
 });
